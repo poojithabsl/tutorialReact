a183dcdba2c2f2f6c49a04e8f04070b6cebef6bb
ReynaRASD <nyan12reyna@gmail.com>
2022-07-01 10:03:06 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import List from './components/List';
+
+const INITIAL_STATE=[
+  {
+    nick: 'Reyna',
+    meses: 6,
+    avatar: 'https://i.pravatar.cc/50',
+    description: 'Nuevo usuario gamer'
+  },
+  {
+    nick: 'Valentine',
+    meses: 9,
+    avatar: 'https://i.pravatar.cc/50',
+  }
+]
+  
+
+ //interface
+interface Sub {
+  nick: string,
+  avatar: string,
+  meses:number,
+  description?: string  //aqui dice qu puede ser opcional la descripion
+}
+
+interface AppState{
+  subs: Array<Sub>
+  newSubsNumber: number
+}
+
 
 function App() {
+  const [number, setNumber]= useState<string|number>(5);
+  const [subs, setSubs] = useState<Array<Sub>>([]);
+  //const [subs, setSubs] = useState<Sub[]>([]);  //esta es otra forma de declararlo
+  const [value, setValue]= useState(0);
+  const [newSubsNumber, setNewSubsNumber]= useState<AppState["newSubsNumber"]>(0);
+
+  const changeNumber=()=>{
+    setNumber("2");
+  }
+
+  useEffect(()=>{
+    setSubs(INITIAL_STATE);
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {number}
+      <br></br>
+      <button onClick={changeNumber}>Change Number</button>
+      <br></br>
+      <h1>Usuarios</h1>
+     <List subs={subs}/>
+      <br>
+      </br>
+      <button onClick={()=>setValue(value+1)}>Incremento</button>
+      <button onClick={()=>setValue(value-1)}>Decremento</button>
+      <h2>{value}</h2>
+
     </div>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,31 @@
+interface Props {
+  subs: Array<{
+    nick: string,
+    avatar: string,
+    meses:number,
+    description?: string
+  }>
+}
+
+
+export default function List({subs}: Props){
+    return(
+        <ul>
+        {
+          subs.map(sub=>{
+            return(
+              
+              <li key={sub.nick}>
+                <img src={sub.avatar} alt={`Avatar for ${sub.nick}`} />
+                <h3>{sub.nick}</h3>
+                <p>{sub.description?.substring(0, 100)}</p>
+                <br></br>
+              </li>
+            )
+          }
+            )
+        }
+      </ul>
+    )
+
+}
